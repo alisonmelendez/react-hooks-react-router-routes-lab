fe555a578baf6cb19dd3ec836f610f8428e790f0
Alison Melendez <alisonmelendez132@gmail.com>
2022-02-13 23:14:55 -0500

    commit tree a6ce440933c746a0194e68c0aa87ed06e8e37082

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+const movieItems =  movies.map((movie)=>{
+          <div key={movie.title} > 
+                <h2>{movie.time}</h2>
+                <ul>
+                  {movies.map((movie)=>
+                      <li key={movie.genres}>{movie.genres}</li> //need only the genres
+                  )}
+                </ul>
+          </div>
+  })
+
+  return <div>
+    <h1>Movies Page </h1>
+    {movieItems}
+  </div>;
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <NavLink>Home</NavLink>
+    <NavLink>Movies</NavLink>
+    <NavLink>Directors</NavLink>
+    <NavLink>Actors</NavLink>
+  </div>;
 }
 
 export default NavBar;
